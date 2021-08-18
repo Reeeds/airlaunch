67469d9a697b07c7cb689c084906c4d26ba0a5c1
reeeds <reto.schuermann@gmail.com>
2021-08-18 17:54:20 +0200

    cron

--- stocks_taskflow_api_etl.py
+++ stocks_taskflow_api_etl.py
@@ -21,7 +21,7 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
-@dag(default_args=default_args, schedule_interval="0 16 * * *", start_date=days_ago(2), tags=['example'])
+@dag(default_args=default_args, schedule_interval="0 16 * * 1,2,3,4,5", start_date=days_ago(2), tags=['example'])
 def stocks_taskflow_api_etl():
 
     @task()

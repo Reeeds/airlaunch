cd3e3f897578006eb5cdafc1491a543df830e451
Reto Schürmann <reto.schuermann@gmail.com>
2021-09-09 16:41:25 +0200

    pre test1

--- pre.py
+++ pre.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 import pandas as pd
 #import os 
 #import glob
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import association_rules, apriori
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 #from airflow.utils.email import send_email
@@ -20,6 +22,9 @@ default_args = {
 }
 
 
+minSupport = Variable.get("minSupport")
+
+
 @dag(default_args=default_args, schedule_interval="* * * * *", start_date=days_ago(2), tags=['example'])
 def pre():
 
@@ -27,10 +32,9 @@ def pre():
     def extractData():
 
         dfDataSalDocsTest = Variable.get("dfDataSalDocsTestJSON", deserialize_json=True)
-        print(type(dfDataSalDocsTest))
-        print(dfDataSalDocsTest)
-#        dfDataSalDocs = pd.read_json(dfDataSalDocsTest)
+
         dfDataSalDocs = pd.DataFrame(dfDataSalDocsTest)
+
         dfDataSalDocs = dfDataSalDocs.groupby('SalDoc_InternalNo')['SalDocItem_ArtInternalNo']
         dataSalDocsList = []
         for name, items in dfDataSalDocs:
@@ -40,21 +44,35 @@ def pre():
                 itemsList.append(str(item))
             dataSalDocsList.append(itemsList)
         
-        print(dataSalDocsList[:10])
+        return dataSalDocsList
 
-#    @task()
-#    def sendEmail():
-#        files = glob.glob("test/*.pdf")  
-#        print(files)
-#        content = '<h1>Radar</h1><br>' + str(kw) + '<br>' + str(files)
-#        send_email(
-#            to=receiversList[0],
-#            bcc=receiversList[1:],
-#            subject='Radar Test ' + str(kw),
-#            html_content=content,
-#            files=files
-#        )
-    extractData()
+    @task()
+    def transform(data:list):
+        te = TransactionEncoder()
+        te_ary = te.fit_transform(dataSalDocsList, sparse=True)
+        sparse_df = pd.DataFrame.sparse.from_spmatrix(te_ary, columns=te.columns_)
+        print('Python Script: SparseMatrix calculated')
+
+        frequent_itemsets = apriori(sparse_df, min_support=minSupport, use_colnames=True, max_len=2, low_memory=True) # LowMemory muss True sein!
+        #print(frequent_itemsets)
+        print('Python Script: Apriori calculated')
+
+        rules = association_rules(frequent_itemsets, metric='lift', min_threshold=1.0)
+        print('Python Script: AssociationRules calculated')
+
+        aResult = rules.sort_values('conviction', ascending=False)
+        # Umformatieren
+        aResult["antecedents"] = rules["antecedents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+        aResult["consequents"] = rules["consequents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+        df = aResult
+        print(df.head())
+
+
+
+
+
+    data = extractData()
+    transform(data)
 
 pre = pre()
 
